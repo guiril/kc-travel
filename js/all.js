e3d2c79080535284f864031552308eb4d954ef0e
ouiis <s9956013@gm.pu.edu.tw>
2018-09-05 01:20:21 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,94 +1,156 @@
-var data;
-var map;
+var allData;
+var dataArray = [];
 
-// 目前的標記
+var map;
 var markers = [];
-// 目前的地圖資訊
-var currentInfoWindow = '';
+
+var currentPage;
+var totalPage;
 
 var select = document.querySelector('#districtList');
 var title = document.querySelector('h2');
 var list = document.querySelector('.cardList');
 var popular = document.querySelector('.popularArea');
+var page = document.querySelector('.pagination');
 var addMap = document.querySelector('#map');
 
-// 將遠端資料存入 data
+
+// 將遠端資料存入allData
 function getData() {
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open('get', 'https://ouiis.github.io/kc_travel/json/data.json', true);
   xhr.send(null);
 
   xhr.onload = function () {
-    var get = JSON.parse(xhr.responseText);
-    data = get.result.records;
-    for (var i = 0; data.length > i; i++) {
-      loadMarkers(data[i].Py, data[i].Px, data[i].Name);
-    }
+    let get = JSON.parse(xhr.responseText);
+    allData = get.result.records;
   }
 };
 
-// 載入地圖
+// 建立地圖
 function initMap() {
   map = new google.maps.Map(addMap, {
-    center: { lat: 22.8498679, lng: 120.4883959 },
-    zoom: 10
+    center: { lat: 22.624815, lng: 120.301097 },
+    zoom: 11
   });
 };
 
-// 更新景點資訊
-function updateList(e) {
-  var district = e.target.value;
-  var str = '';
-  var total = 0;
+// 地圖標記
+function mapMarker(lat, lng, title) {
+  var marker = new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    title: title,
+    map: map
+  })
+  // 將目前標記存入markers
+  markers.push(marker);
+};
 
-  // 清除原本的標記
+// 更新景點資訊
+function updateList(pageNumber) {
+  let arraySize = dataArray.length;
+
+  let pageSize = 6; // 每頁有6筆資料
+  let rangeStart = (pageNumber - 1) * pageSize;
+  let rangeEnd = pageNumber * pageSize;
+  let lastPageSize = arraySize % pageSize;
+
+  let htmlStr = '';
+  let htmlPage = '';
+
+  // 清空目前標記
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   };
-
   markers = [];
-  infoWindows = [];
 
-  // 載入景點
-  for (var i = 0; i < data.length; i++) {
-    if (district == data[i].Zone) {
-      total += 1;
-      str += '<li><div class="pic" style="background-image: url(' + data[i].Picture1 + ');"><h3>' + data[i].Name + '</h3><span>' + district + '</span></div><div class="txt"><p class="opentime"><span></span>' + data[i].Opentime + '</p><p class="address"><span></span>' + data[i].Add + '</p><p class="tel"><span></span>' + data[i].Tel + '</p><p class="ticket"><span></span>' + data[i].Ticketinfo + '</p></div></li >';
-      loadMarkers(data[i].Py, data[i].Px, data[i].Name);
-    }
-  };
-
-  title.textContent = district;
-  list.innerHTML = str;
-
-  if (total == 0) {
+  // 顯示頁碼
+  if (arraySize == 0) {
     title.textContent = '目前尚無資料';
+    list.innerHTML = '';
+    page.innerHTML = '';
+    return;
+  } else {
+    title.textContent = dataArray[0].Zone; // 標題顯示地區
+    totalPage = Math.ceil(arraySize / pageSize); // 無條件進位計算頁數
+    for (let i = 1; i <= totalPage; i++) {
+      if (i == pageNumber) {
+        htmlPage += `<a href="#" class="page-numbers current" data-pagination=${i}>${i}</a>`;
+      } else {
+        htmlPage += `<a href="#" class="page-numbers" data-pagination=${i}>${i}</a>`;
+      }
+    };
+    page.innerHTML = `<a href="#" class="prev-button"><i class="fas fa-chevron-left" data-pagination="prev"></i></a>${htmlPage}<a href="#" class="last-button"><i class="fas fa-chevron-right" data-pagination="next"></i></a>`;
   };
-}
 
-function loadMarkers(lat, lng, title) {
-  // 地圖資訊
-  var infowindow = new google.maps.InfoWindow({
-    content: title
-  });
-  // 標記
-  var marker = new google.maps.Marker({
-    position: { lat: parseFloat(lat), lng: parseFloat(lng) },
-    title: title,
-    map: map
-  });
+  // 最後一筆資料的索引
+  if (pageNumber == totalPage) {
+    if (lastPageSize != 0) {
+      rangeEnd = rangeStart + lastPageSize;
+    };
+  };
 
-  marker.addListener('click', function () {
-    if (currentInfoWindow != '') {
-      currentInfoWindow.close();
-      currentInfoWindow = '';
-    }
-    infowindow.open(map, marker);
-    currentInfoWindow = infowindow;
-  });
-  markers.push(marker);
-}
+  // 顯示資料
+  for (let i = rangeStart; i < rangeEnd; i++) {
+    htmlStr += `<li><div class="pic" style="background-image: url(${dataArray[i].Picture1});"><h3>${dataArray[i].Name}</h3><span>${dataArray[i].Zone}</span></div><div class="txt"><p class="opentime"><span></span>${dataArray[i].Opentime}</p><p class="address"><span></span>${dataArray[i].Add}</p><p class="tel"><span></span>${dataArray[i].Tel}</p><p class="ticket"><span></span>${dataArray[i].Ticketinfo}</p></div></li>`;
+    mapMarker(parseFloat(dataArray[i].Py), parseFloat(dataArray[i].Px), dataArray[i].Name);
+  };
+
+  list.innerHTML = htmlStr;
+  currentPage = pageNumber;
+};
+
+function getValue(e) {
+  e.preventDefault();
+  let district = e.target.value;
+  dataArray = [];
+
+  if (e.target.nodeName == 'INPUT' || e.target.nodeName == 'SELECT') {
+    for (let i = 0; i < allData.length; i++) {
+      if (district == allData[i].Zone) {
+        dataArray.push(allData[i]);
+      };
+    };
+  } else {
+    return;
+  };
+
+  updateList(1);
+};
+
+function getPageNumber(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName == 'A' || e.target.nodeName == 'I') {
+    let pagination = e.target.dataset.pagination;
+
+    if (pagination == 'prev' || pagination == 'next') {
+      if (pagination == 'prev') {
+        if (currentPage == 1) {
+          return;
+        };
+        pagination = currentPage - 1;
+      }
+      if (pagination == 'next') {
+        if (currentPage == totalPage) {
+          return;
+        }
+        pagination = currentPage + 1;
+      }
+    };
+
+    updateList(pagination);
+  }
+};
+
+$('.goTop a').click(function (e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: 0
+  }, 800);
+})
 
 getData();
-select.addEventListener('change', updateList, false);
-popular.addEventListener('click', updateList, false);
+select.addEventListener('change', getValue, false);
+popular.addEventListener('click', getValue, false);
+page.addEventListener('click', getPageNumber, true);
